b8b55efdae592ba472a3280a0cef3d661d3aefa7
jcchen.code <jcchen.code@gmail.com>
2020-11-20 10:41:02 +0800

    generate password

---
+++ user-customs/funs.el
@@ -0,0 +1,31 @@
+
+(defun random-alnum ()
+  (let* ((alnum "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+         (i (% (abs (random)) (length alnum))))
+    (substring alnum i (1+ i))))
+
+
+(defun random-string (n)
+  "Generate a slug of n random alphanumeric characters.
+Inefficient implementation; don't use for large n."
+  
+  (if (= 0 n)
+      ""
+    (concat (random-alnum) (random-string (1- n)))))
+
+
+(defun gen-password (&optional n)
+  "Generate password. Default length is 12"
+  (interactive (list (if current-prefix-arg ; <== User provide arg 
+			 (prefix-numeric-value current-prefix-arg)
+		       12))) 		; <== Default
+  (insert (random-string n)))
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Prefix-Command-Arguments.html
+
+
+
+
+
+
+
+
